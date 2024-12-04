d835911780ee3099ceca99c505e627c7146b6465
Tanmayi tadala <tanmayitadala@gmail.com>
2024-12-04 21:04:44 +0530

    UC1:Initialized Snake and Ladder game with single player starting at position 0

--- src/SnakeAndLadderProblem.java
+++ src/SnakeAndLadderProblem.java
@@ -1,5 +1,8 @@
 public class SnakeAndLadderProblem {
     public static void main(String[] args) {
         System.out.println("This problem simulates a Snake and Ladder Game.");
+        //UC1
+        int playerPosition = 0; // Starting position
+        System.out.println("Game Started: Player at position " + playerPosition);
     }
 }
