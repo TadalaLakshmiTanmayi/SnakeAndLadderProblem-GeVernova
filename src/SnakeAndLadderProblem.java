8adf0152ce5467ae2a735840e50f765ea53de7e8
Tanmayi tadala <tanmayitadala@gmail.com>
2024-12-04 21:11:25 +0530

    UC2:Implemented player die roll using RANDOM to generate a number between 1 and 6

--- src/SnakeAndLadderProblem.java
+++ src/SnakeAndLadderProblem.java
@@ -1,8 +1,15 @@
+import java.util.Random;
+
 public class SnakeAndLadderProblem {
     public static void main(String[] args) {
         System.out.println("This problem simulates a Snake and Ladder Game.");
         //UC1
         int playerPosition = 0; // Starting position
         System.out.println("Game Started: Player at position " + playerPosition);
+        Random random = new Random();
+
+        // UC2: Player rolls the die to get a number between 1 to 6
+        int diceRoll = random.nextInt(6) + 1; // Random number between 1 and 6
+        System.out.println("Rolled the die: " + diceRoll);
     }
 }
